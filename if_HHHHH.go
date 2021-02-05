2d059ddf6ad213b643c930495ef8ba7d3a19dc5f
“rahulitibd” <rahulitibd@gmail.com>
2021-02-05 10:51:00 +0600

    If_Statement

---
+++ if_HHHHH.go
@@ -0,0 +1,20 @@
+package main
+import "fmt"
+func main(){
+	/*if 9%2==0{
+		fmt.Println("9 is even")
+	}else{
+		fmt.Println("9 is odd")
+	}*/
+	//if 8%2==0{
+		//fmt.Println("8 is odd")
+	//}else{
+		//fmt.Println("8 is even")
+	//}
+if num:=10; num>0{
+	fmt.Println(num, "is negative ")
+}else if num>10{
+	fmt.Println(num, " is posetive")
+	}
+
+}
