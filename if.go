43bc289e7ced4dfc538ac59c4e8a0bf47eb9723f
“rahulitibd” <rahulitibd@gmail.com>
2021-02-05 10:37:38 +0600

    If statement

---
+++ if.go
@@ -0,0 +1,14 @@
+package main
+import "fmt"
+func main(){
+	/*if 9%2==0{
+		fmt.Println("9 is even")
+	}else{
+		fmt.Println("9 is odd")
+	}*/
+	if 8%2==0{
+		fmt.Println("8 is odd")
+	}else{
+		fmt.Println("8 is even")
+	}
+}
