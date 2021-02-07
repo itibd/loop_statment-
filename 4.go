761826877c21a0af8f6fd3e249fe78ce772b7d9a
“rahulitibd” <rahulitibd@gmail.com>
2021-02-07 13:33:27 +0600

    practis_m

---
+++ 4.go
@@ -0,0 +1,13 @@
+package main
+import "fmt"
+func main(){
+	var x int64=325
+	y:=32
+	z:="my name is rahul islam"
+	fmt.Println(x)
+	fmt.Println(y)
+	fmt.Println(z)
+	fmt.Println(x+y	)
+	fmt.Printf("z is of this variable is %T",z)
+	
+}
